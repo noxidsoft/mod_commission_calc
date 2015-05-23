3a8939bb82adfa58aeeb75bd8557180a3d051885
Noel Dixon <noel.dixon@noxidsoft.com>
2015-05-23 21:48:46 +1000

    Initial commit

---
+++ js/ajax.js
@@ -0,0 +1,67 @@
+/**
+ * version      $Id$
+ * package      Commission Calculator - Dollar
+ * subpackage   mod_commission_calc_dollar
+ * copyright    Copyright (C) 2009 Noxidsoft. All rights reserved.
+ * license      GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ * author       Noel Dixon <noel.dixon@noxidsoft.com>
+ */
+
+var xmlhttp
+
+function showCalc1(str)
+{
+var url = location.href;
+var base1 = url.substring(0, url.indexOf('/', 14));
+var baseURL = base1 + "/";
+
+if (str.length==0)
+  {
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+xmlhttp=GetXmlHttpObject();
+if (xmlhttp==null)
+  {
+  alert ("Your browser does not support XMLHTTP!");
+  return;
+  }
+var percentBefore 	= document.getElementById("percentBefore").value;
+var baseAmount 		= document.getElementById("baseAmount").value;
+var percentAfter 	= document.getElementById("percentAfter").value;
+var taxation 		= document.getElementById("taxation").value;
+var url=baseURL + "modules/mod_commission_calc_dollar/nonstdhelper.php";
+url=url+"?q="+str;
+url=url+"&sid="+Math.random();
+url=url+"&percentBefore="+percentBefore;
+url=url+"&baseAmount="+baseAmount;
+url=url+"&percentAfter="+percentAfter;
+url=url+"&taxation="+taxation;
+xmlhttp.onreadystatechange=stateChanged1;
+xmlhttp.open("GET",url,true);
+xmlhttp.send(null);
+}
+
+function stateChanged1()
+{
+if (xmlhttp.readyState==4)
+  {
+  document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+  document.getElementById('txt2').value = document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+  }
+}
+
+function GetXmlHttpObject()
+{
+if (window.XMLHttpRequest)
+  {
+  // code for IE7+, Firefox, Chrome, Opera, Safari
+  return new XMLHttpRequest();
+  }
+if (window.ActiveXObject)
+  {
+  // code for IE6, IE5
+  return new ActiveXObject("Microsoft.XMLHTTP");
+  }
+return null;
+}
---
+++ mod_commission_calc_dollar.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * @version      $Id$
+ * @package      Commission Calculator - Dollar
+ * @subpackage   mod_commission_calc_dollar
+ * @copyright    Copyright (C) 2009 Noxidsoft. All rights reserved.
+ * @license      GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ * @author       Noel Dixon <noel.dixon@noxidsoft.com>
+ */
+
+//don't allow other scripts to grab and execute our file
+defined('_JEXEC') or die();
+
+$baseUrl = JUri::base(true);
+
+$salePriceLabel		= $params->get( 'salePriceLabel' );
+$commissionLabel	= $params->get( 'commissionLabel' );
+$labelPosition		= $params->get( 'labelPosition' );
+$percentBefore		= $params->get( 'percentOfAmount' );
+$baseAmount 		= $params->get( 'amount' );
+$percentAfter		= $params->get( 'percentOfEverythingAfterAmount' );
+$taxation 			= $params->get( 'taxation' );
+$style				= $params->get( 'style' );
+?>
+
+<html>
+<head>
+<title></title>
+<jdoc:include type="head" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="<?php echo $baseUrl ?>/modules/mod_commission_calc_dollar/css/template.css" type="text/css" />
+<?php
+	$document = &JFactory::getDocument();
+	$document->addScript( $baseUrl.'/modules/mod_commission_calc_dollar/js/ajax.js' );
+?>
+</head>
+
+<!-- Default - Below Fields -->
+<?php if ($labelPosition == 0) { ?>
+	<!-- Default - Inline Pretty -->
+	<?php if ($style == 0) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr>
+			<td><input type="text" id="txt1" class="inputBox" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><input type="text" id="txt2" class="inputBox" maxlength="10" /></td>
+		</tr>
+		<tr class="textStyle">
+			<td><?php echo "$salePriceLabel"; ?></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><?php echo "$commissionLabel"; ?></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+
+	<!-- Inline Raw -->
+	<?php if ($style == 1) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr>
+			<td><input type="text" id="txt1" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><input type="text" id="txt2" maxlength="10" /></td>
+		</tr>
+		<tr>
+			<td><?php echo "$salePriceLabel ($)"; ?></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><?php echo "$commissionLabel ($)"; ?></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+
+	<!-- Stacked Pretty -->
+	<?php if ($style == 2) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr>
+			<td><input type="text" id="txt1" class="inputBox" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+		</tr>
+		<tr class="textStyle">
+			<td><?php echo "$salePriceLabel"; ?></td>
+		</tr>
+		<tr class="textStyle">
+			<td><br /></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt2" class="inputBox" maxlength="10" /></td>
+		</tr>
+		<tr class="textStyle">
+			<td><?php echo "$commissionLabel"; ?></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+
+	<!-- Stacked Raw -->
+	<?php if ($style == 3) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr>
+			<td><input type="text" id="txt1" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+		</tr>
+		<tr>
+			<td><?php echo "$salePriceLabel ($)"; ?></td>
+		</tr>
+		<tr>
+			<td><br /></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt2" maxlength="10" /></td>
+		</tr>
+		<tr>
+			<td><?php echo "$commissionLabel ($)"; ?></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+<?php } ?>
+
+<!-- Above Fields -->
+<?php if ($labelPosition == 1) { ?>
+	<!-- Default - Inline Pretty -->
+	<?php if ($style == 0) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr class="textStyle">
+			<td><?php echo "$salePriceLabel"; ?></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><?php echo "$commissionLabel"; ?></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt1" class="inputBox" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><input type="text" id="txt2" class="inputBox" maxlength="10" /></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+
+	<!-- Inline Raw -->
+	<?php if ($style == 1) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr>
+			<td><?php echo "$salePriceLabel ($)"; ?></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><?php echo "$commissionLabel ($)"; ?></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt1" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+			<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			<td><input type="text" id="txt2" maxlength="10" /></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+
+	<!-- Stacked Pretty -->
+	<?php if ($style == 2) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr class="textStyle">
+			<td><?php echo "$salePriceLabel"; ?></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt1" class="inputBox" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+		</tr>
+		<tr>
+			<td><br /></td>
+		</tr>
+		<tr class="textStyle">
+			<td><?php echo "$commissionLabel"; ?></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt2" class="inputBox" maxlength="10" /></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+
+	<!-- Stacked Raw -->
+	<?php if ($style == 3) { ?>
+		<body>
+		<form>
+		<table border="0">
+		<tr>
+			<td><?php echo "$salePriceLabel ($)"; ?></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt1" onkeyup="showCalc1(this.value)" maxlength="10" /></td>
+		</tr>
+		<tr>
+			<td><br /></td>
+		</tr>
+		<tr>
+			<td><?php echo "$commissionLabel ($)"; ?></td>
+		</tr>
+		<tr>
+			<td><input type="text" id="txt2" maxlength="10" /></td>
+		</tr>
+		</table>
+		<input type="hidden" id="percentBefore" value="<?php echo $percentBefore; ?>" />
+		<input type="hidden" id="baseAmount" value="<?php echo $baseAmount; ?>" />
+		<input type="hidden" id="percentAfter" value="<?php echo $percentAfter; ?>" />
+		<input type="hidden" id="taxation" value="<?php echo $taxation; ?>" />
+		</form>
+		<div class="txtHint"><span id="txtHint"></span></div>
+		</body>
+	<?php } ?>
+<?php } ?>
+
+</html>
