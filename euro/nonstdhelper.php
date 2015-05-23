fb0ebce89ee6eefec56d3c766d73518543d85b39
Noel Dixon <noel.dixon@noxidsoft.com>
2015-05-23 22:09:56 +1000

    Initial commit

---
+++ euro/nonstdhelper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version      $Id$
+ * @package      Commission Calculator - Euro
+ * @subpackage   mod_commission_calc_euro
+ * @copyright    Copyright (C) 2009 Noxidsoft. All rights reserved.
+ * @license      GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ * @author       Noel Dixon <noel.dixon@noxidsoft.com>
+ */
+
+//get the q parameter from URL which governs the users input
+$q 				= $_GET["q"];
+$percentBefore 	= $_GET["percentBefore"];
+$baseAmount 	= $_GET["baseAmount"];
+$percentAfter 	= $_GET["percentAfter"];
+$taxation		= $_GET["taxation"];
+
+// do the forward calculations
+if ($q > 0) {
+	if ($q > $baseAmount) {
+		$response = $baseAmount * ($percentBefore / 100) + ($q - $baseAmount) * ($percentAfter / 100);
+		// add tax at the end of everything
+		$tax = $response * ($taxation / 100);
+		// send the result to the screen
+		//echo $response + $tax;
+		echo number_format($response + $tax, 2);
+	} else {
+		$response = $q * ($percentBefore / 100);
+		// add tax at the end of everything
+		$tax = $response * ($taxation / 100);
+		// send the result to the screen
+		//echo $response + $tax;
+		echo number_format($response + $tax, 2);
+	}
+}
